61eca404ed6328712d2a955b54cc4ec3fa2202e7
r.y <itworldbird@gmail.com>
2020-07-12 20:06:54 +0900

    auto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if (message.image) {
      var html =
-     `<div class="message">
+     `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
              ${message.user_name}
@@ -22,7 +22,7 @@ $(function(){
      return html;
    } else {
      var html =
-     `<div class="message">
+     `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
              ${message.user_name}
@@ -42,11 +42,9 @@ return html;
    };
  }
 $('#new_message').on('submit', function(e){
- 
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action');
- console.log(url);
  $.ajax({
    url: url,
    type: "POST",
@@ -55,17 +53,40 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
  })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('form')[0].reset();
-    $('input').prop('disabled', false);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight})
-    
-    
-  })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
+ .done(function(data){
+  var html = buildHTML(data);
+  $('.messages').append(html);
+  $('form')[0].reset();
+  $('input').prop('disabled', false);
+  $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight})
+ 
   });
-})
+});
+
+var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
